347bdac73c9d63cb4e22cf7830499adbe1f2cc19
JessicaA-CodeCrew <ms.jessica.avarie@gmail.com>
2019-08-12 12:15:05 -0500

    Week 1 Review

---
+++ index.js
@@ -0,0 +1,21 @@
+var userInput = "";
+while (userInput !== 'q')
+{
+    userInput = prompt("Enter 1 for Count Down or Enter 2 for Count Up");
+
+    if (userInput === "1")
+    {
+        alert ("You've entered 1")
+    }
+    else if (userInput === "2")
+    {
+        alert("You've Entered 2'")
+    }
+}
+
+// // var counter = 0;
+// // while (counter > 0)
+// {
+//(
+// }
+
